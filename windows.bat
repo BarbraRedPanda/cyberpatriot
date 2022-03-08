fe5457277bba5de1f5a36a99dbe1790d3948effb
BarbraRedPanda <parsak9000@gmail.com>
2022-03-07 19:24:02 -0600

    Divided account policy into 2 settings, added show hidden folder setting, cleaned up code

--- windows.bat
+++ windows.bat
@@ -3,12 +3,14 @@ goto :main
 #----------------------------------------
 #   BarbraRedPanda
 #   2022 Cyberpatriot Windows 10 Script
+#   https://github.com/BarbraRedPanda/cyberpatriot/
 #----------------------------------------
 
 :main
 @echo off
+color 
 
-rem checks if there are sufficient privs (https://github.com/ponkio/CyberPatriot/blob/master/Windows.bat) 
+rem checks if there are sufficient privs 
 net session
 if %errorlevel%==0 (
 	echo "We're in. hacking itno mainframe. initiansting fortniteburger."
@@ -66,26 +68,34 @@ goto :launch
     echo "#                            'We like fortnite'                               #"
     echo "#                     -Tyler 'Fortnite' 'Ninja' Blevins                       #"
     echo "#-----------------------------------------------------------------------------#"
-    echo "# (1) account policy  (2) audit  (3) firewall                                              #"
+    echo "#      (1) password policy  (2) lockout policy  (3) audit (4) firewall      #"
+    echo "#      (5) show hidden folders        #"
     echo "#-----------------------------------------------------------------------------#"
     set /p choice = choose an option: 
-        if "%choice%"=="1" goto :acctPol
-        if "%choice%"=="2" goto :audit
-        if "%choice%"=="2" goto :firewall
+        if "%choice%"=="1" goto :passPol
+        if "%choice%"=="1" goto :lockPol
+        if "%choice%"=="3" goto :audit
+        if "%choice%"=="4" goto :firewall
+        if "%choice%"=="5" goto :hidden
 
-:acctPol
-Rem changes pass reqs
+:passPol
+    Rem changes pass reqs
     net accounts /MINPWLEN:8
     net accounts /MAXPWAGE:30
     net accounts /MINPWAGE:15
+    net accounts /UNIQUEPW:24
     Set-ADDefaultDomainPasswordPolicy -ComplexityEnabled true
     Set-ADDefaultDomainPasswordPolicy -ReversibleEncryptionEnabled false
     Set-ADDefaultDomainPasswordPolicy -PasswordHistoryCount 24
-Rem changes lockout stuff
-    Set-ADDefaultDomainPasswordPolicy -LockoutThreshold 5
-    Set-ADDefaultDomainPasswordPolicy -LockoutDuration 0.0:30:0.0
-    Set-ADDefaultDomainPasswordPolicy -LockoutObservationWindow 0.0:30:0.0
-    
+
+:lockPol
+    Rem changes lockout stuff
+        rem Set-ADDefaultDomainPasswordPolicy -LockoutThreshold 5
+        rem Set-ADDefaultDomainPasswordPolicy -LockoutDuration 0.0:30:0.0
+        rem Set-ADDefaultDomainPasswordPolicy -LockoutObservationWindow 0.0:30:0.0
+    net accounts /LOCKOUTTHRESHOLD:5
+    net accounts /LOCKOUTDURATION:30
+    net accounts /LOCKOUTWINDOW:30
     pause
     goto :launch
 
@@ -100,4 +110,13 @@ Rem changes lockout stuff
 :firewall
     netsh advfirewall set allprofiles state on
     pause
+    goto :launch
+
+:hidden
+
+    reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v Hidden /t REG_DWORD /d 1 /f
+    reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v ShowSuperHidden /t REG_DWORD /d 1 /f
+    powershell -c gps 'explorer' ^| stop-process
+
+    pause   
     goto :launch
