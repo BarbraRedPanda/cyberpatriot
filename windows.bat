cdb9d8b7235028fa43e63a75d16173257be2ef51
BarbraRedPanda <parsak9000@gmail.com>
2022-03-09 22:12:10 -0600

    Fixed wording

--- windows.bat
+++ windows.bat
@@ -72,7 +72,7 @@ if "%forensics%"=="n" (
     echo #-------------------------------------------------------------------------------#
     echo #       (0) password policy  (1) lockout policy  (2) audit (3) firewall         #
     echo #       (4) hidden folders   (5) remote console  (6) remote access              #
-    echo #       (7) guest/admin      (8) telnet          (9) findBad                    #
+    echo #       (7) guest/admin      (8) telnet.         (9) search and destroy         #
     echo #-------------------------------------------------------------------------------#
     set /p choice=choose an option: 
         timeout /t 1
