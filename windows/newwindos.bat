747b5cfe6c0ed40593ebea01bbf4dbfdbd5304f8
BarbraRedPanda <parsak9000@gmail.com>
2022-10-29 10:30:02 -0500

    organized branches

---
+++ windows/newwindos.bat
@@ -0,0 +1,14 @@
+   goto :main; 
+    
+    
+   echo #-------------------------------------------------------------------------------#
+   echo #-                     Cybervores CyberPatriot Script                          -#
+   echo #-------------------------------------------------------------------------------#
+   echo #-                            'We like fortnite'                               -#
+   echo #-                     -Tyler 'Fortnite' 'Ninja' Blevins                       -#
+   echo #-------------------------------------------------------------------------------#
+   echo #       (0) password policy  (1) lockout policy  (2) audit (3) firewall         #
+   echo #       (4) hidden folders   (5) remote console  (6) remote access              #
+   echo #       (7) guest/admin      (8) telnet.         (9) search and destroy         #
+   echo #-------------------------------------------------------------------------------#
+ 
